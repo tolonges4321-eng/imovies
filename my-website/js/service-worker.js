9f0c371c6cf7bc1edf85d81ffff7e70db0f7fb51
tolonges4321-eng <tolonges4321@gmail.com>
2026-01-05 09:30:40 +0800

    service-worker.js

--- my-website/js/service-worker.js
+++ my-website/js/service-worker.js
@@ -1,1 +1,21 @@
+const CACHE_NAME = "imovies-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/css/home.css",
+  "/js/home.js",
+  "/logo.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
 
