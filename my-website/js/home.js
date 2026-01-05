b66783778c6adf3d52d47ee0ffc9d453f18cbcea
tolonges4321-eng <tolonges4321@gmail.com>
2026-01-05 09:28:29 +0800

    home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -133,7 +133,17 @@ function startBannerAutoPlay() {
   }, 5000);
 }
 
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+  toggle.textContent =
+    document.body.classList.contains("light") ? "☀️" : "🌙";
+});
+
+
 
     init();
+
 
 
